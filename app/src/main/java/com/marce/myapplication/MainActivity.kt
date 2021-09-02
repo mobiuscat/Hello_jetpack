3d58d21132a3d6c3c36faebb2a1fa644cd973a6f
Baliani Marcelo <baliani.m85@gmail.com>
2021-09-01 21:30:26 -0300

    added animations

--- app/src/main/java/com/marce/myapplication/MainActivity.kt
+++ app/src/main/java/com/marce/myapplication/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.marce.myapplication
 
 import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -25,7 +31,8 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             Hello_jetpackTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+//                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -37,28 +44,47 @@ fun MessageCard(msg:Message){
     Row (modifier = Modifier.padding(all = 8.dp)){
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            contentDescription = null,
             modifier = Modifier
                 // Set image size to 40 dp
                 .size(40.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)
-                .border(1.5.dp,MaterialTheme.colors.secondary, CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(text = msg.author,
-                color= MaterialTheme.colors.secondaryVariant
+                color= MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+
             )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium,elevation = 1.dp){
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+                // animateContentSize will change the surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp),
+            ){
                 Text(text = msg.body,
                     modifier = Modifier.padding(all=4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
@@ -66,17 +92,35 @@ fun MessageCard(msg:Message){
     }
 }
 
-@Preview(name = "Dark mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    showBackground = true,
-    name = "Light Mode"
-)
 @Composable
-fun PreviewMessageCard(){
-    Hello_jetpackTheme {
-        MessageCard(
-            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-        )
+fun Conversation(messages:List<Message>){
+    LazyColumn{
+        items(messages){
+                message -> MessageCard(message)
+        }
     }
 }
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    Hello_jetpackTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+
+//@Preview(name = "Dark mode")
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_NO,
+//    showBackground = true,
+//    name = "Light Mode"
+//)
+//@Composable
+//fun PreviewMessageCard(){
+//    Hello_jetpackTheme {
+//        MessageCard(
+//            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+//        )
+//    }
+//}
